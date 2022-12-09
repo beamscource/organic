5a630509156a9e25f8a6edeb9e74399dac984410
Eugen Klein <klein.eug@gmail.com>
2022-12-09 14:28:48 +0100

    There are no stable YT download libraries

--- sample/download_audio_yt.py
+++ sample/download_audio_yt.py
@@ -1,14 +1,14 @@
 
 import argparse
-import pafy
-
 
 def download(channel_list):
 
     for channel in channel_list:
+
+        breakpoint()
         pass
 
-def parse(channel_file):
+def parse_file(channel_file):
 
     with open(channel_file, 'r', encoding='utf-8') as f:
         channel_list = f.readlines()
@@ -16,16 +16,20 @@ def parse(channel_file):
     return channel_list
 
 def main(args):
-    pass
+    
+    channel_file = args.channel_file
+    
+    channel_list = parse_file(channel_file)
+    download(channel_list)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 
     '''Download and rip audio from Youtube channels using specific keywords.'''
     
-    parser = argparse.ArgumentParser(description='Provide a text file containing
+    parser = argparse.ArgumentParser(description='Provide a text file containing \
         a list of relevant channels.')
     
-    parser.add_argument('-ch', '--channel_file', required=True, help='Text file
+    parser.add_argument('-ch', '--channel_file', required=True, help='Text file \
         containing a list with YT channels.')
     
     args = parser.parse_args()
